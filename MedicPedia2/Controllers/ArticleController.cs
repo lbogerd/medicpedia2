b623c07b838849c03bd00129c3150f058d94c927
semih <semih@vybe-it.nl>
2018-03-28 13:15:43 +0200

    All Controllers Added

---
+++ MedicPedia2/Controllers/ArticleController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedicPedia2.Controllers
+{
+    public class ArticleController : Controller
+    {
+
+        Article SingleArticle;
+        SqlArticleRepository articleRepository = new SqlArticleRepository();
+        SqlCategoryRepository categoryRepository = new SqlCategoryRepository();
+
+        // GET: Article
+        public ActionResult Index()
+        {
+            var articleList = articleRepository.GetArticleList();
+            return View(articleList);
+        }
+
+        public ActionResult Article(Guid id)
+        {
+            var articleList = articleRepository.GetArticleList();
+
+            for (int i = 0; i < articleList.Count; i++)
+            {
+                if (articleList[i].Id == id)
+                { this.SingleArticle = articleList[i]; }
+            }
+
+            return View(SingleArticle);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            Article article = new Article();
+            return View(article);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Article article)
+        {
+            articleRepository.Add(article);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Update(Guid articleId)
+        {
+            var articleToUpdate = articleRepository.Get(articleId);
+            return View(articleToUpdate);
+        }
+
+        [HttpPost]
+        public ActionResult Update(Article article)
+        {
+            articleRepository.Update(article);
+
+            return RedirectToAction("Article");
+        }
+
+    }
+}
---
+++ MedicPedia2/Controllers/AuthorController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MedicPedia2.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private SqlAuthorRepository AuthorRepo = new SqlAuthorRepository();
+
+        [HttpGet]
+        public ActionResult Index() => View(AuthorRepo.GetAll());
+
+        [HttpGet]
+        public ActionResult Create() => View();
+
+        [HttpPost]
+        public ActionResult Create(Author author)
+        {
+            AuthorRepo.Add(author);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Update(Guid id) => View(AuthorRepo.Get(id));
+
+        [HttpPost]
+        public ActionResult Update(Author author)
+        {
+            AuthorRepo.Update(author);
+            return RedirectToAction("Index");
+        }
+    }
+}

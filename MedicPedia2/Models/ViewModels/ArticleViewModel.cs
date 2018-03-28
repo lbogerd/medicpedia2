4bd90f4a8f06b0d9c059868ff4e0dea57f9b28c7
Lucas Bogerd <l.bogerd@protonmail.com>
2018-03-28 14:07:19 +0200

    added article vm

---
+++ MedicPedia2/Models/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MedicPedia2.Models.ViewModels
+{
+    public class ArticleViewModel
+    {
+        public Article Article { get; set; }
+        public List<Category> AllPossibleCategories { get; set; }
+    }
+}

55bc5a055ff8f8961ac17bf8e33fff9d03c16ddf
semih <semih@vybe-it.nl>
2018-03-28 13:32:13 +0200

    using MedicPedia2.Models; using MedicPedia2.Repositories.ArticleRepository;
    added in all controller files

--- MedicPedia2/Controllers/ArticleController.cs
+++ MedicPedia2/Controllers/ArticleController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MedicPedia2.Models;
+using MedicPedia2.Repositories.ArticleRepository;
 
 namespace MedicPedia2.Controllers
 {
